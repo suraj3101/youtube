4ee65169d915ce045dd0d76ebfc219f2fcf67e42
Suraj Gupta <surajgupta3101@gmail.com>
2024-09-09 16:24:23 +0530

    Optimized Search Bar

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HamburgerIcon from "../assest/hamburger-icon.jpg";
 import YoutubeLogo from "../assest/YouTube-Logo.png";
 import UserIcon from "../assest/UserIcon.png";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
+import { YOUTUBE_SEARCH_API } from "../utils/constant";
+import { cacheResults } from "../utils/searchSlice";
 
 const Head = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
+  const searchCache = useSelector((store) => store.search);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (searchCache[searchQuery]) {
+        setSuggestions(searchCache[searchQuery]);
+      } else {
+        getSearchSuggestions();
+      }
+    }, 200);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSuggestions = async () => {
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+    setSuggestions(json[1]);
+
+    dispatch(
+      cacheResults({
+        [searchQuery]: json[1],
+      })
+    );
+  };
 
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
@@ -28,14 +61,31 @@ const Head = () => {
       </div>
 
       {/* Section-2 */}
-      <div className="col-span-10 px-10 text-center">
-        <input
-          className="w-1/2 border border-gray-400 rounded-l-full"
-          type="text"
-        />
-        <button className="border border-gray-400 rounded-r-full bg-gray-100">
-          Search
-        </button>
+      <div className="col-span-10 px-10">
+        <div>
+          <input
+            className="px-5 w-1/2 border border-gray-400 rounded-l-full"
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
+          />
+          <button className="border border-gray-400 rounded-r-full bg-gray-100">
+            Search
+          </button>
+        </div>
+        {showSuggestions && (
+          <div className="fixed bg-white py-2 px-2 w-[27.5rem] shadow-lg rounded-lg border border-gray-100">
+            <ul>
+              {suggestions.map((s) => (
+                <li key={s} className="py-2 px-3 shadow-sm hover:bg-gray-100">
+                  {s}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
 
       {/* Section-3 */}
--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { YOUTUBE_VIDEOS_API } from "../utils/constant";
-import VideoCard from "./VideoCard";
+import VideoCard, {AdVideoCard} from "./VideoCard";
 import { Link } from "react-router-dom";
 
 const VideoContainer = () => {
@@ -19,8 +19,9 @@ const VideoContainer = () => {
 
   return (
     <div className="flex flex-wrap">
+      {videos[0] && <AdVideoCard info={videos[0]}/>}
       {videos.map((video) => (
-        <Link to={"/watch?v=" + video.id}>
+        <Link key={video.id} to={"/watch?v=" + video.id}>
           <VideoCard info={video} key={video.id} />
         </Link>
       ))}
